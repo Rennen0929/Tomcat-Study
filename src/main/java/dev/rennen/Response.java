16bf9d7e9add972b7e4cbe40c527ccd4da8a321e
ZhangXu <zxkl1314@gmail.com>
2024-01-08 10:04:02 +0800

    编写发送响应行

--- src/main/java/dev/rennen/Response.java
+++ src/main/java/dev/rennen/Response.java
@@ -2,6 +2,7 @@ package dev.rennen;
 
 import javax.servlet.ServletOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,7 +10,16 @@ public class Response extends AbstractHttpServletResponse{
 
     private int status = 200;
     private String message = "OK";
+    private byte SP = ' ';
+    private byte CR = '\r';
+    private byte LF = '\n';
     private Map<String, String> headers = new HashMap<>();
+    private Request request;
+    private OutputStream socketOutputStream;
+    public Response(Request request) {
+        this.request = request;
+
+    }
 
     @Override
     public void setStatus(int i, String s) {
@@ -34,6 +44,30 @@ public class Response extends AbstractHttpServletResponse{
 
     public void complete() {
         // 发送响应
-        
+        try {
+            sendResponseLine();
+            sendResponseHeader();
+            sendResponseBody();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private void sendResponseBody() {
+    }
+
+    private void sendResponseHeader(){
+        }
+
+
+    private void sendResponseLine() throws IOException {
+        socketOutputStream.write(request.getProtocol().getBytes());
+        socketOutputStream.write(SP);
+        socketOutputStream.write(status);
+        socketOutputStream.write(SP);
+        socketOutputStream.write(message.getBytes());
+        socketOutputStream.write(CR);
+        socketOutputStream.write(LF);
     }
 }
