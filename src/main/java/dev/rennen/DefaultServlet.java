fbd803ee83ee53528e216d1afcc58ddd44cd74c8
Xia Jiacheng <rennen0929@gmail.com>
2024-01-11 21:21:05 +0800

    feat: 添加 404 页面

--- src/main/java/dev/rennen/DefaultServlet.java
+++ src/main/java/dev/rennen/DefaultServlet.java
@@ -1,6 +1,18 @@
 package dev.rennen;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 
 public class DefaultServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        OutputStream outputStream = resp.getOutputStream();
+        outputStream.write("404 页面未找到".getBytes(StandardCharsets.UTF_8));
+    }
 }
