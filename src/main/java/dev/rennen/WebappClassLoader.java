96e4a2c9fcb8cbf78acf80fb88e25e3c53e3fd4f
ZhangXu <zxkl1314@gmail.com>
2024-01-11 16:42:44 +0800

    自定义类加载器

---
+++ src/main/java/dev/rennen/WebappClassLoader.java
@@ -0,0 +1,10 @@
+package dev.rennen;
+
+import java.net.URL;
+import java.net.URLClassLoader;
+
+public class WebappClassLoader extends URLClassLoader {
+    public WebappClassLoader(URL[] urls) {
+        super(urls);
+    }
+}
